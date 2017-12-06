0071da80093ca37829c060c6836bf65f9a8e6c9a
taqwa <trushdan@gmail.com>
2017-12-05 23:25:14 -0500

    -m "latest edits"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,101 +1,97 @@
 
-// Hangman pseudocode
-
-//Game instructions
-  //type in letters to guess the random word.
-
-//Wins
-  //Logs how many words that the user guessed correctly, without running out of letter turns
-  //When word is guessed, the answer is displayed at the top, and a photo appears.
-  //function repeats itself for the next word.
-
-//Current word
-  //Generates the word that the user must guess
-
-//Guesses remaining
-  //Shows the user how many guesses they have for a given word
-
-//Letters already guessed
-  //displays all of the letters/keys that the user has already pressed that were NOT part of the word
-  //if the letter typed in IS in word, then it is moved to be displayed in the appropriate empty space.
-
-
-//Array of words that we'll use--
-  //var beeWords = ["queen", "worker", "drone", "pollination", "beyonce", "swarm", "metamorphosis", "royaljelly"]
-
-
-  //correctGuesses
-  //wrongGuesses
-  //guessesRemaining
-
-  // input -- user is asked to guess a letter.
-  // user's input is then compared to a string of letters.
-  //if the user's input matches any of the letters in the string -- the matching letter will be displayed in blank(s).
-  //if the user's input does not match any of the letter in the string -- the letter that they typed in will be displayed in the 'letters guessed already' section.
-    //once guessed, a letter cannot be guessd again
-  //after each time the user plays a letter, the counter for 'number of guesses remaining' will decrease by 1
-
-
 //create an array of beeWords
   var beeWords = ["queen", "worker", "drone", "pollination", "beyonce", "swarm", "metamorphosis", "royaljelly"];
 
 /////choose word randomly
   var randomNum = Math.floor(Math.random() * beeWords.length);
   var randomWord = beeWords[randomNum].toUpperCase();
+
   var rightWord = [];
   var wrongWord = [];
   var underScore = []; // empty array for underscores
+  var userGuesses = 12;
 
-console.log(randomWord); //to test randomWord var
+//to test randomWord var
+  console.log(randomWord);
+
+/////Dom manipulation
+  var underScoreDiv = document.getElementById('underscores');
+  var correctGuessDiv = document.getElementById('correct-guesses');
+  var wrongGuessDiv = document.getElementById('wrong-guesses');
+  var guessRemainDiv = document.getElementById('guesses-remaining');
+  var lettersGuessedDiv = document.getElementById('letters-guessed');
+
 
 /////create underscores based on length of random word that we generated
+// --> makeUnderscore function start
   function makeUnderscore() {
     for (var i = 0; i < randomWord.length; i++) {
       underScore.push("_");
     }
     return underScore;
   }
+//--> makeUnderscore function end
 
-console.log(makeUnderscore()); // testing
+// testing
+  console.log(makeUnderscore());
 
-/////get users guess
+//calling startGame function
+  startGame();
+
+//defining startGame function
+  function startGame() {
+
+/////purpose: get users letter(s) guess
+//--> event keyup function start
   document.addEventListener('keyup', (function (event) {
+
   //converts unicode key value into a string
     var userKeyInput = String.fromCharCode(event.keyCode);
     console.log(userKeyInput);
 
   // if users guess is right
     if (randomWord.indexOf(userKeyInput) > -1) {
+
     //add to rightWord array
       rightWord.push(userKeyInput);
-    //replace underscore with right letter
-      underScore[randomWord.indexOf(userKeyInput)] = userKeyInput;
 
+    //replace underscore with right letter
+      underScore[randomWord.indexOf(userKeyInput)] = userKeyInput; // place for loop around this
+
+    //dom manipulation
+      underScoreDiv.innerHTML = underScore.join(' ');
+      correctGuessDiv.innerHTML = rightWord.join(''); //make this into number of wins if all letters guess correctly.
+
+    //checks if user's word matches guesses
       if (underScore.join('') == randomWord) {
         console.log(rightWord);
-        //console.log('you win');
-        //need to figure out how to make repeated letter show up and be logged with one keypress
-        alert('you win');
+        console.log('you win');
+
+  //** need to figure out how to make repeated letter show up and be logged with one keypress **//
+
+      //calling function to restart game after word is guessed.
+        startGame()
       }
 
-      // wrongWord.push(userKeyInput);
+    } else {
+    //adds wrong guesses to wrongWord array
+      wrongWord.push(userKeyInput);
+
+    //DOM minpulation. joins all wrong guesses together in an array and places it in the lettersGuessedDiv
+      lettersGuessedDiv.innerHTML = wrongWord;
+
+    //if wrongWord array input is greater than 12 create message saying no more guesses, and 'stop' function
+      //**This if statement is not working**//
+      if (wrongWord.join('') > 12) {
+        console.log('no guesses remaining');
+        startGame()
+      }
+      // wrongWord if statement end
     }
+    //else statement end
 
 }));
+//--> event keyup function end
 
-//console.log(userKeyInput.indexOf(randomWord));
-  //console.log(rightWord);
-  //console.log(wrongWord);
-
-  //store user's input
-
-
-  //check if right
-  //if right display in blank space
-  //if wrong, display in 'letters guessed'
-  //don't allow a letter already guessed to be entered again
-
-
-//check if guess is right
-//if right push to right array
-//if wrong push to wrong array
+}
+//startGame function end
