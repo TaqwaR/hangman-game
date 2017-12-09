5a2609e26304da2e3ae1a8166b856cc0201f7889
taqwa <trushdan@gmail.com>
2017-12-09 00:00:21 -0500

    troubleshooting restart

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,84 +1,102 @@
 
-/////create an array of beeWords
-  var beeWords = [ "wings", "hive", "wax", "drone", "nectar", "queen", "wasp", "sting", "flower", "forage", "comb", "solitary"];
+/////create array beeWords
+var beeWords = [ "wings", "hive", "wax", "drone", "nectar", "queen", "wasp", "sting", "flower", "forage", "comb", "solitary"];
 
-  var beeWordsPlus2 = ["apiary","worker", "pollination", "beyonce", "swarm", "metamorphosis", "royaljelly", "pupa", "buzz"];
+var beeWordsPlus2 = ["apiary","worker", "pollination", "beyonce", "swarm", "metamorphosis", "royaljelly", "pupa", "buzz"];
 
 /////choose word randomly
-  var randomNum = Math.floor(Math.random() * beeWords.length);
-  var randomWord = beeWords[randomNum].toUpperCase();
-
+var randomNum = Math.floor(Math.random() * beeWords.length);
+var randomWord = beeWords[randomNum].toUpperCase();
 
 ////words and guesses
-  var rightWord = [];
-  var wrongWord = [];
-  var underScore = []; // empty array for underscores
-
+var rightWord = [];
+var wrongWord = [];
+var underScore = [];
 
 //to test randomWord var
-  console.log(randomWord);
+console.log(randomWord);
 
 /////Dom manipulation
-  var resultDiv = document.getElementById('game-result');
-  var underScoreDiv = document.getElementById('underscores');
-  var correctGuessDiv = document.getElementById('correct-guesses');
-  var wrongGuessDiv = document.getElementById('wrong-guesses');
-  var guessRemainDiv = document.getElementById('guesses-remaining');
-  var lettersGuessedDiv = document.getElementById('letters-guessed');
+var resultDiv = document.getElementById('game-result');
+var underScoreDiv = document.getElementById('underscores');
+var correctGuessDiv = document.getElementById('correct-guesses');
+var wrongGuessDiv = document.getElementById('wrong-guesses');
+var guessRemainDiv = document.getElementById('guesses-remaining');
+var lettersGuessedDiv = document.getElementById('letters-guessed');
 
 /////Counters
-  var winCounter = 0;
-  var loseCounter = 0;
-  var guessRemainCounter = 10;
+var winCounter = 0;
+var loseCounter = 0;
+var guessRemainCounter = 10;
+
+////restart game on click function
+document.getElementById('new-word').addEventListener("click", (function restartGame(event) {
+
+  rightWord = [];
+  wrongWord = [];
+  underScore = [];
+
+  loseCounter = 0;
+  guessRemainCounter = 10;
+  //gameKeyup(event); <-- cannot get this to work
+  makeUnderscore();
+
+  wrongGuessDiv.innerHTML = loseCounter;
+  guessRemainDiv.innerHTML = guessRemainCounter;
+  lettersGuessedDiv.innerHTML = wrongWord;
+  underScoreDiv.innerHTML = underScore.join(" ");
+
+}
+
+
+)
+
+);
 
 
 /////create underscores based on length of random word that we generated
-  function makeUnderscore() {
-    for (var i = 0; i < randomWord.length; i++) {
-      underScore.push("_");
-    }
-    document.getElementById('underscores').innerHTML = underScore.join(" ");
-
-    //for testing
-    return underScore
+function makeUnderscore() {
+  for (var i = 0; i < randomWord.length; i++) {
+    underScore.push("_");
   }
+  document.getElementById('underscores').innerHTML = underScore.join(" ");
+  //for testing
+  return underScore
+}
 
 // testing
-  console.log(makeUnderscore());
+console.log(makeUnderscore());
 
 /////get users letter(s) guess
-  document.addEventListener('keyup', (function gameKeyup(event) {
+document.addEventListener('keyup', (function gameKeyup(event) {
 
-  //converts unicode key value into a string
-    var userKeyInput = String.fromCharCode(event.keyCode);
-    console.log(userKeyInput);
+//converts unicode key value into a string
+  var userKeyInput = String.fromCharCode(event.keyCode);
+  console.log(userKeyInput);
 
-  // if users guess is right
-    if (randomWord.indexOf(userKeyInput) > -1) {
+// if users guess is right
+  if (randomWord.indexOf(userKeyInput) > -1) {
+    rightWord.push(userKeyInput);
 
-    //add to rightWord array
-      rightWord.push(userKeyInput);
-
-    //checking for more than one occurence of a letter
-      for (var i = 0; i < underScore.length; i++) {
-        underScore[randomWord.indexOf(userKeyInput)] = userKeyInput;
-
-        if (userKeyInput[i] = randomWord[i]) {
-          userKeyInput[i] = underScore[underScore.indexOf(i)];
-        }
+  //checking for more than one occurence of a letter
+    for (var i = 0; i < underScore.length; i++) {
+      underScore[randomWord.indexOf(userKeyInput)] = userKeyInput;
+      if (userKeyInput[i] = randomWord[i]) {
+        userKeyInput[i] = underScore[underScore.indexOf(i)];
       }
+    }
 
-    //DOM manipulation - joining each generated underscore/typed letter with a space between them.
-      underScoreDiv.innerHTML = underScore.join(' ');
+//DOM manipulation - joining each generated underscore/typed letter with a space between them.
+  underScoreDiv.innerHTML = underScore.join(' ');
 
-    //If the letters typed in the underscore spaces jointly equal the random word, stop keyUp events and display the following
-      if (underScore.join('') == randomWord) {
-        document.removeEventListener('keyup', gameKeyup);
-        document.getElementById('game-result').innerHTML = "That's right. You guessed " + randomWord;
-        document.getElementById('correct-guesses').innerHTML = ++winCounter;
-        console.log(rightWord);
-        console.log('you win');
-      }
+//If the letters typed in the underscore spaces jointly equal the random word, stop keyUp events and display the following
+    if (underScore.join('') == randomWord) {
+      document.removeEventListener('keyup', gameKeyup);
+      document.getElementById('game-result').innerHTML = "That's right. You guessed " + randomWord;
+      document.getElementById('correct-guesses').innerHTML = ++winCounter;
+      console.log(rightWord);
+      console.log('you win');
+    }
 
   // if the joint letters do NOT equal the random word
     } else {
@@ -102,18 +120,3 @@
     }
 
 }));
-
-/////restart game function
-  // document.getElementById('new-word').addEventListener("click", function restartGame() {
-  //     makeUnderscore();
-  //     rightWord = [];
-  //     wrongWord = [];
-  //     underScore = [];
-  //
-  //     // winCounter++;
-  //     loseCounter = 0;
-  //     guessRemainCounter = 10;
-  //
-  // });
-  //
-  // restartGame("click");
